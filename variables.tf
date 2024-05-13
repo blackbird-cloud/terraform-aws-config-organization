33a2b4b393ec62a7ff49ab0408f9c894c897dfa8
Joeri Malmberg <joeri.malmberg@blackbird.cloud>
2024-05-13 18:58:25 +0200

    Bring your own s3 bucket, and iam role.

--- variables.tf
+++ variables.tf
@@ -10,26 +10,18 @@ variable "conformance_packs" {
   description = "(Optional) Map of AWS Config Organization Conformance Packs. More information can be found in the Managing Conformance Packs Across all Accounts in Your Organization and AWS Config Managed Rules documentation."
 }
 
-variable "administrator_arns" {
-  type        = list(string)
-  description = "List of AWS principals that will receive Administrative permissions on the resources created."
-  default     = []
-}
-
-variable "viewers_arns" {
-  type        = list(string)
-  description = "(Optional) List of AWS principals that will receive viewing permissions on the Config bucket."
-  default     = []
-}
-
-variable "writers_arns" {
-  type        = list(string)
-  description = "(Optional) List of AWS principals that will receive writing permissions on the Config bucket."
-  default     = []
-}
-
 variable "tags" {
   type        = map(string)
   description = "(Optional) Map of tags to assign to the trail. If configured with a provider `default_tags` configuration block present, tags with matching keys will overwrite those defined at the provider-level."
   default     = {}
+}
+
+variable "s3_bucket_name" {
+  type        = string
+  description = "The name of the S3 bucket to store Config history."
+}
+
+variable "role_arn" {
+  type        = string
+  description = "The ARN of the IAM role to use for this configuration recorder."
 }
